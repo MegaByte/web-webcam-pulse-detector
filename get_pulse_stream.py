8bf07612fb92f410ea09e2bcf0999f6cc2d295e2
Aaron Kaluszka <aaron@lumoslabs.com>
2014-05-15 22:59:49 -0700

    Add simple HTTP server

--- get_pulse_stream.py
+++ get_pulse_stream.py
@@ -3,7 +3,7 @@ from lib.processors import findFaceGetPulse
 from lib.interface import plotXY, imshow, waitKey,destroyWindow, moveWindow, resize
 import numpy as np
 import sys
-
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 
 class getPulseApp(object):
     """
@@ -41,7 +41,7 @@ class getPulseApp(object):
         #(A GUI window must have focus for these to work)
         self.key_controls = {"s" : self.toggle_search,
                         "d" : self.toggle_display_plot}
-
+        self.bpm = 0
 
     def toggle_search(self):
         """
@@ -109,7 +109,7 @@ class getPulseApp(object):
         # Get current image frame from the camera
         (frame, frame_time) = self.stream.get_frame()
         if frame is None:
-            return True
+            return False
         self.h,self.w,_c = frame.shape
 
         #display unaltered frame
@@ -131,17 +131,44 @@ class getPulseApp(object):
         #create and/or update the raw data display if needed
         # if self.bpm_plot:
         #     self.make_bpm_plot()
-        print str(self.processor.time_in) + '\t' + str(self.processor.fft.samples[-1])
+        self.bpm = self.processor.fft.samples[-1]
+        print str(self.processor.time_in) + '\t' + str(self.bpm)
 
         return True
+
+class MyHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        if self.path == '/crossdomain.xml':
+            self.send_header('Content-type', 'text/xml')
+            self.end_headers()
+            self.wfile.write('<cross-domain-policy><site-control permitted-cross-domain-policies="master-only"/><allow-access-from domain="*"/><allow-http-request-headers-from domain="*" headers="*"/></cross-domain-policy>')
+        else:
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            next_frame = App.main_loop()
+            while next_frame:
+                next_frame = App.main_loop()
+            if self.path == '/':
+                self.wfile.write(str(App.bpm))
+            else:
+                self.wfile.write(self.path[1:] + "({bpm: " + str(App.bpm) + "})")
+        return
+    def do_POST(self):
+        self.send_response(200)
+        # dump image into processing directory?
+        return
 
 if __name__ == "__main__":
     # example (replace these values)
     if len(sys.argv) < 2:
         raise Exception("Specify a directory for the webcam stream")
     App = getPulseApp(sys.argv[1])
-    next_frame = App.main_loop()
-    while next_frame:
-        next_frame = App.main_loop()
-    App.print_data()
 
+    try:
+        server = HTTPServer(('', 3001), MyHandler)
+        server.serve_forever()
+    except KeyboardInterrupt:
+        App.print_data()
+        server.socket.close()
